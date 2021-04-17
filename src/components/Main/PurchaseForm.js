3031d6102922e64118c8aa9c3ff252ede264323c
sunhpark42 <sunhpark42@gmail.com>
2021-04-17 17:42:49 +0900

    refactor: PurchaseForm 을 function componenent로 변경

--- src/components/Main/PurchaseForm.js
+++ src/components/Main/PurchaseForm.js
@@ -1,6 +1,62 @@
-import { Component } from 'react';
-import { ID, LOTTO_PRICE, MESSAGE } from '../../constants';
+import { useState } from 'react';
+import { LOTTO_PRICE, MESSAGE } from '../../constants';
 
+export default function PurchaseForm(props) {
+  const [isActive, setIsActive] = useState(true);
+  const [purchaseAmount, setPurchaseAmount] = useState(0);
+
+  const onPurchaseAmountChange = (event) => {
+    setPurchaseAmount(event.target.valueAsNumber);
+  };
+
+  const setLottoCount = (count) => {
+    props.setLottoCount(count);
+  };
+
+  const onSubmitPurchaseForm = (event) => {
+    event.preventDefault();
+
+    if (!isValidPrice()) {
+      alert(MESSAGE.ALERT.INVALID_MONEY_UNIT);
+      return;
+    }
+
+    setLottoCount(purchaseAmount / LOTTO_PRICE);
+    setIsActive(false);
+  };
+
+  const isValidPrice = () => {
+    return purchaseAmount % LOTTO_PRICE === 0;
+  };
+
+  return (
+    <section className="mt-5">
+      <form className="w-100" onSubmit={onSubmitPurchaseForm}>
+        <label htmlFor="money-input" className="w-100">
+          구입할 금액을 입력해주세요. (단위: 원)
+        </label>
+        <div className="d-flex justify-space-between items-center mt-1">
+          <input
+            id="money-input"
+            className="money-input flex-auto mr-3"
+            type="number"
+            placeholder="구입 금액"
+            min="1000"
+            value={purchaseAmount || ''}
+            onChange={onPurchaseAmountChange}
+            required
+            disabled={isActive ? false : true}
+          />
+          <button type="submit" className="basic-button" disabled={isActive ? false : true}>
+            확인
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+}
+
+/*
 export default class PurchaseForm extends Component {
   state = {
     isActive: true,
@@ -58,3 +114,4 @@ export default class PurchaseForm extends Component {
     );
   }
 }
+*/
